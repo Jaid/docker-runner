b118236f38c9c4a5b33046582130ddf1836652c4
Jaid <jaid.jsx@gmail.com>
2023-01-01 19:06:44 +0100

    Fixed bash binary path
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/entrypoint.bash
+++ src/entrypoint.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 chmod ugo+x /bin/run.bash
-/usr/bin/su --command '/usr/bin/bash -o errexit -o pipefail -o xtrace /bin/run.bash' "${userName:=$(id -nu 1000)}"
+/usr/bin/su --command '/bin/bash -o errexit -o pipefail -o xtrace /bin/run.bash' "${userName:=$(id -nu 1000)}"
